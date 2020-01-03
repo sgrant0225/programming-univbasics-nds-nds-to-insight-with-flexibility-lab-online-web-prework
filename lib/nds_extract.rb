5c0e38a36f4bde8eeaf9dd57d2bace72104d21b1
Sharnell Grant <sharnellgrant@gmail.com>
2020-01-03 21:54:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -89,7 +89,7 @@ end
 def movies_with_directors_set(source)
   new_array = []
   row _index = 0 
-  while row_index < source.length do 
+  while source.length do 
     name = source[row_index][:name] 
     directors_movies = source[row_index][:movies]
     new_array << movies_with_directors_key(name, directors_movies)
