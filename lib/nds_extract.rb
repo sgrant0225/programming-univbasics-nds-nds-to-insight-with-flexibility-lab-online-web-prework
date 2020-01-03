e1f1d1a287994246e92f259eb4dce0f41406fd82
Sharnell Grant <sharnellgrant@gmail.com>
2020-01-03 22:02:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -88,7 +88,13 @@ end
 
 def movies_with_directors_set(source)
   new_array = []
-  
+   
+  source.each do |movies_info|
+    movies_info_with_director = movies_with_director_key(movies_info[:name], movies_info[:movies])
+    new_arr << movies_info_with_director
+  end #each
+  new_array
+end 
   
 end
   # GOAL: For each director, find their :movies Array and stick it in a new Array
